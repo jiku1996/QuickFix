486c6cb53425ff2bc6999d3d959db0adca6385e0
PicQue <49796510+PicQue@users.noreply.github.com>
2019-06-24 17:24:51 +0800

    Update addWheels.cs

--- Assets/addWheels.cs
+++ Assets/addWheels.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class addWheels : MonoBehaviour
 {
+//this script is to add a new wheel from player to the broken car
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +17,17 @@ public class addWheels : MonoBehaviour
         
     }
 
+
     private void OnTriggerStay(Collider other)
     {
+    //checks if tage of the objects is Player
         if (other.tag == "Player")
         {
+        //gets a script attached on the player called objectLocation
             objectLocation pOL = other.GetComponent<objectLocation>();
             if (pOL.sendTag() == "tire")
             {
+            //gameObject becomes the new parent of objectLocation obj
                 pOL.giveObject(gameObject);
             }
         }
